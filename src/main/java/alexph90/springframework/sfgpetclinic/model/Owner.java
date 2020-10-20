7f3655f472c1abd8c1c4833577a8803319b4d663
Alex <alexph90@gmail.com>
2020-10-20 14:16:35 +0100

    Adding POJOs. Closes #2.

---
+++ src/main/java/alexph90/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package alexph90.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/alexph90/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package alexph90.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
