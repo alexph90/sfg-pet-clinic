70162d34b1277b3e60c39c6650f4e7b290085ed8
Alex <alexph90@gmail.com>
2020-11-14 18:18:31 +0000

    Closes #29

---
+++ pet-clinic-data/src/main/java/alexph90/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package alexph90.springframework.sfgpetclinic.services;
+
+import alexph90.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
