56725037df3e9d46088128e843d3e1285f7df24b
Alex <alexph90@gmail.com>
2020-11-14 18:57:04 +0000

    Closes #31

---
+++ pet-clinic-data/src/main/java/alexph90/springframework/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package alexph90.springframework.sfgpetclinic.services;
+
+import alexph90.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/alexph90/springframework/sfgpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,37 @@
+package alexph90.springframework.sfgpetclinic.services.map;
+
+import alexph90.springframework.sfgpetclinic.model.Speciality;
+import alexph90.springframework.sfgpetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
