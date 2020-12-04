133ee7d0e3e64a9de7c79982d5707a571963b4ce
Alex <alexph90@gmail.com>
2020-12-04 15:56:20 +0000

    Closes #46

---
+++ pet-clinic-data/src/main/java/alexph90/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package alexph90.springframework.sfgpetclinic.services;
+
+import alexph90.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
